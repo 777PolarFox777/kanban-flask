0c9cd6316dbb628bb81b40459d95069f3993721e
Andrew <Andrew5282011@gmail.com>
2021-06-01 00:24:39 +0300

    Serve static files from frontend

--- backend/app.py
+++ backend/app.py
@@ -1,5 +1,6 @@
 import os
 import flask
+from flask import send_from_directory
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from backend.utils import CustomJsonEncoder
@@ -14,3 +15,13 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 migrate = Migrate(app, db)
+
+
+# noinspection PyBroadException
+@app.route("/<path:filename>")
+def fallback(filename):
+    public_dir = os.path.abspath("../frontend/dist/")
+    try:
+        return send_from_directory(public_dir, path=filename)
+    except Exception:
+        return send_from_directory(public_dir, path="index.html")
